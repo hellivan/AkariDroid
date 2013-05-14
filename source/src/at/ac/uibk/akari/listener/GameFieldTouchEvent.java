809c90c6e76bd1d24d98132db8de6e5579502f95
Ivan Hell <hell.ivan@yahoo.com>
2013-05-14 12:02:57 +0200

    Changed scroll capability from 2 fingers to 1. Also fixed bug of mandatary online-state when starting the game

--- source/src/at/ac/uibk/akari/listener/GameFieldTouchEvent.java
+++ source/src/at/ac/uibk/akari/listener/GameFieldTouchEvent.java
@@ -1,27 +1,20 @@
 package at.ac.uibk.akari.listener;
 
 import android.graphics.Point;
-import android.graphics.PointF;
 import at.ac.uibk.akari.view.GameField;
 
 public class GameFieldTouchEvent {
 
 	private GameField source;
 	private Point touchCell;
-	private PointF touchPosition;
 
-	public GameFieldTouchEvent(final GameField source, final Point touchCell, final PointF touchPosition) {
+	public GameFieldTouchEvent(final GameField source, final Point touchCell) {
 		this.source = source;
 		this.touchCell = touchCell;
-		this.touchPosition = touchPosition;
 	}
 
 	public Point getTouchCell() {
 		return this.touchCell;
-	}
-
-	public PointF getTouchPosition() {
-		return this.touchPosition;
 	}
 
 	public GameField getSource() {
